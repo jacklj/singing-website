eec52ed313856721c2d0cf812213fe8397af474a
Jack Lawrence-Jones <jackvlj@gmail.com>
2018-05-21 16:09:54 +0100

    Add server.js

---
+++ server/server.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+
+const port = process.env.PORT || 8080;
+
+http.listen(port, function(){
+  console.log(`hello.\nlistening on ${port}.`);
+});
+
+app.get('/', function (req, res) {
+  res.send("__hi__");
+});
