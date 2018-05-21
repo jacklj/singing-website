faf967f3f0f71abc37fd9e81e144656d3bf43ddb
Jack Lawrence-Jones <jackvlj@gmail.com>
2018-05-21 16:14:32 +0100

    Serve static files from frontend directory

--- server/server.js
+++ server/server.js
@@ -4,6 +4,8 @@ var http = require('http').Server(app);
 
 const port = process.env.PORT || 8080;
 
+app.use(express.static('../frontend'));
+
 http.listen(port, function(){
   console.log(`hello.\nlistening on ${port}.`);
 });
