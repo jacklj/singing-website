c6aedc59e6bf225f8d5b1b3677d6da4b626c9664
Jack Lawrence-Jones <jackvlj@gmail.com>
2018-05-21 17:49:02 +0100

    Attempted heroku relative file static serving fix

--- server/server.js
+++ server/server.js
@@ -1,12 +1,19 @@
 var express = require('express');
 var app = express();
 var http = require('http').Server(app);
+const path = require('path');
 
 const port = process.env.PORT || 8080;
 
-app.use(express.static('../frontend'));
-app.use('/blog', express.static('../blog/public'));
+const staticDir = path.join(__dirname, '..', 'frontend');
+const blogStaticDir = path.join(__dirname, '..', 'blog', 'public');
 
+console.log('__dirname: ', __dirname)
+console.log('staticDir: ', staticDir)
+console.log('blogStaticDir: ', blogStaticDir)
+
+app.use(express.static(staticDir));
+app.use('/blog', express.static(blogStaticDir));
 
 http.listen(port, function(){
   console.log(`hello.\nlistening on ${port}.`);
