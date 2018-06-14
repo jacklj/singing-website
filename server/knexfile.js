1b4a4177be037ac6713185fb4db99a8c15a02e00
Jack Lawrence-Jones <jackvlj@gmail.com>
2018-06-14 23:46:02 +0100

    Remove unused knexfile staging config

--- server/knexfile.js
+++ server/knexfile.js
@@ -11,23 +11,6 @@ module.exports = {
       directory: __dirname + '/db/seeds/development'
     }
   },
-
-  staging: {
-    client: 'pg',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  },
-
   production: {
     client: 'pg',
     connection: {
