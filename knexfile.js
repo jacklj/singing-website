490ea096568e4def693a62a200088c16f5065c9d
Jack Lawrence-Jones <jackvlj@gmail.com>
2018-06-15 21:54:15 +0100

    Simplify knex production connection config

--- knexfile.js
+++ knexfile.js
@@ -13,9 +13,7 @@ module.exports = {
   },
   production: {
     client: 'pg',
-    connection: {
-      database: process.env.DATABASE_URL,
-    },
+    connection: process.env.DATABASE_URL,
     ssl: true,
     pool: {
       min: 2,
