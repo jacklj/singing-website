d8533dfcc3bb32d868edcee9d7ebf5ef82b085fd
Jack Lawrence-Jones <jackvlj@gmail.com>
2018-06-15 23:28:41 +0100

    Contact form

--- server.js
+++ server.js
@@ -2,8 +2,12 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 const path = require('path');
-const knex = require('./db/knex.js');
+require('dotenv').config()
+const nodemailer = require('nodemailer');
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: true}));
 
+const knex = require('./db/knex.js');
 const port = process.env.PORT || 8080;
 
 // need to use __dirname so that these paths are correct in Heroku
@@ -21,4 +25,32 @@ app.get('/events', function(req, res, next) {
   knex('events').select()
     .then(events => res.status(200).json(events))
     .catch(error => console.warn(error));
+});
+
+// POST route from contact form
+app.post('/contact', function (req, res) {
+  let mailOpts, smtpTrans;
+  smtpTrans = nodemailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true,
+    auth: {
+      user: process.env.GMAIL_USER,
+      pass: process.env.GMAIL_PASS
+    }
+  });
+  mailOpts = {
+    from: req.body.name + ' &lt;' + req.body.email + '&gt;',
+    to: process.env.GMAIL_USER,
+    subject: 'New message from contact form at jacklawrencejones.com',
+    text: `${req.body.name} (${req.body.email}) says: ${req.body.message}`
+  };
+  smtpTrans.sendMail(mailOpts, function (error, response) {
+    if (error) {
+      res.send('contact-failure');
+    }
+    else {
+      res.send('contact-success');
+    }
+  });
 });
