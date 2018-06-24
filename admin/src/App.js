d4f46bdebb6de471e5feb277f0ff7defbc6c0ac8
Jack Lawrence-Jones <jackvlj@gmail.com>
2018-06-24 16:00:51 +0100

    Production edit and create admin pages

--- admin/src/App.js
+++ admin/src/App.js
@@ -4,13 +4,13 @@ import jsonServerProvider from 'ra-data-json-server';
 
 import dataProvider from './dataProvider';
 import { VenuesList, VenueEdit, VenueCreate } from './venues';
-import { ProductionsList } from './productions';
+import { ProductionsList, ProductionEdit, ProductionCreate } from './productions';
 import { ShowsList, ShowEdit, ShowCreate } from './shows';
 
 const App = () => (
   <Admin dataProvider={dataProvider}>
     <Resource name="venues" list={VenuesList} edit={VenueEdit} create={VenueCreate} />
-    <Resource name="productions" list={ProductionsList} />
+    <Resource name="productions" list={ProductionsList} edit={ProductionEdit} create={ProductionCreate} />
     <Resource name="shows" list={ShowsList} edit={ShowEdit} create={ShowCreate} />
   </Admin>
 );
--- admin/src/productions.js
+++ admin/src/productions.js
@@ -1,6 +1,6 @@
 // in src/posts.js
 import React from 'react';
-import { List, Datagrid, TextField } from 'react-admin';
+import { List, Datagrid, TextField, Edit, SimpleForm, DisabledInput, TextInput, LongTextInput, Create, EditButton } from 'react-admin';
 
 export const ProductionsList = (props) => (
     <List {...props}>
@@ -11,6 +11,36 @@ export const ProductionsList = (props) => (
             <TextField source="url" />
             <TextField source="copy" />
             <TextField source="my_role" />
+            <EditButton />
         </Datagrid>
     </List>
+);
+
+const ProductionTitle = ({ record }) => {
+    return <span>Production {record ? `"${record.name}"` : ''}</span>;
+};
+
+export const ProductionEdit = (props) => (
+    <Edit title={<ProductionTitle />} {...props}>
+        <SimpleForm>
+            <DisabledInput source="id" />
+            <TextInput source="name" />
+            <TextInput source="company" />
+            <TextInput source="url" />
+            <LongTextInput source="copy" />
+            <TextInput source="my_role" />
+        </SimpleForm>
+    </Edit>
+);
+
+export const ProductionCreate = (props) => (
+    <Create {...props}>
+        <SimpleForm>
+          <TextInput source="name" />
+          <TextInput source="company" />
+          <TextInput source="url" />
+          <LongTextInput source="copy" />
+          <TextInput source="my_role" />
+        </SimpleForm>
+    </Create>
 );
