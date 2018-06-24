70b9a00971b4db655d4dcac7fa60da37d722b2a3
Jack Lawrence-Jones <jackvlj@gmail.com>
2018-06-24 23:12:51 +0100

    Migration create users table

---
+++ db/migrations/20180624225955_create-users-table.js
@@ -0,0 +1,13 @@
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('users', function(table){
+    table.increments('id').primary();
+    table.string('username').notNullable();
+    table.string('token');
+    table.string('password_digest');
+    table.timestamp('created_at').defaultTo(knex.fn.now());
+  });
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTable('users');
+};
