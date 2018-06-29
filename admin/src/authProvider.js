2c9f2e801031726e1d1935424922e693c72eca5e
Jack Lawrence-Jones <jackvlj@gmail.com>
2018-06-30 00:34:40 +0100

    Prettier

--- admin/src/authProvider.js
+++ admin/src/authProvider.js
@@ -1,38 +1,38 @@
-import { AUTH_LOGIN, AUTH_LOGOUT, AUTH_ERROR } from 'react-admin';
+import { AUTH_LOGIN, AUTH_LOGOUT, AUTH_ERROR, AUTH_CHECK } from 'react-admin';
 
 export default (type, params) => {
-    if (type === AUTH_LOGIN) {
-        const { username, password } = params;
-        const request = new Request('./api/signin', {
-            method: 'POST',
-            body: JSON.stringify({ username, password }),
-            headers: new Headers({ 'Content-Type': 'application/json' }),
-        })
-        return fetch(request)
-            .then(response => {
-                if (response.status < 200 || response.status >= 300) {
-                    throw new Error(response.statusText);
-                }
-                return response.json();
-            })
-            .then(({ token }) => {
-                localStorage.setItem('token', token);
-            });
+  if (type === AUTH_LOGIN) {
+    const { username, password } = params;
+    const request = new Request('./api/signin', {
+      method: 'POST',
+      body: JSON.stringify({ username, password }),
+      headers: new Headers({ 'Content-Type': 'application/json' }),
+    });
+    return fetch(request)
+      .then(response => {
+        if (response.status < 200 || response.status >= 300) {
+          throw new Error(response.statusText);
+        }
+        return response.json();
+      })
+      .then(({ token }) => {
+        localStorage.setItem('token', token);
+      });
+  }
+  if (type === AUTH_LOGOUT) {
+    // ...
+  }
+  if (type === AUTH_ERROR) {
+    // see https://marmelab.com/admin-on-rest/Authentication.html#catching-authentication-errors-on-the-api
+    const status = params.status;
+    if (status === 401 || status === 403) {
+      localStorage.removeItem('token');
+      return Promise.reject();
     }
-    if (type === AUTH_LOGOUT) {
-       // ...
-   }
-   if (type === AUTH_ERROR) {
-       const status  = params.status;
-       if (status === 401 || status === 403) {
-           localStorage.removeItem('token');
-           return Promise.reject();
-       }
-       return Promise.resolve();
-      // see https://marmelab.com/admin-on-rest/Authentication.html#catching-authentication-errors-on-the-api
-   }
+    return Promise.resolve();
+  }
   if (type === AUTH_CHECK) {
     return localStorage.getItem('token') ? Promise.resolve() : Promise.reject();
   }
-   return Promise.resolve();
-}
+  return Promise.resolve();
+};
